13b176fba57813b095f1f47301a3d744075ef063
Ash Bauer <105770584+halfbakedbread@users.noreply.github.com>
2023-09-03 12:09:20 -0500

    feat: integrate home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,16 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Banfts Marketplace</h1>
+      <p class="py-6">Buy and sell Banano NFTs securetly and efforlessly! No registration required</p>
+      <a class="btn btn-primary" href="/auth/representative">Auth with representative</a>
+      <a class="btn btn-primary" href="/auth/signature">Auth with signature</a>
+    </div>
+  </div>
+</div>
 
 <style lang="postcss">
   :global(html) {
-    background-color: theme(colors.gray.100);
+    background-color: theme("colors.gray.100");
   }
 </style>
