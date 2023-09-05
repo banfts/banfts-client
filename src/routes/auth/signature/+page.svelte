9fc81dc57515a60c8f1a36b75a3a33907fe99dbf
Ash Bauer <105770584+halfbakedbread@users.noreply.github.com>
2023-09-05 10:04:00 -0500

    feat: integrate signature auth page

---
+++ src/routes/auth/signature/+page.svelte
@@ -0,0 +1,32 @@
+<div class="flex flex-grow my-4">
+  <div class="hero min-h-screen bg-base-300">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+      <div class="text-center lg:text-left">
+        <h1 class="text-5xl font-bold">Authenticate to Banfts!</h1>
+        <p class="py-6">Sign a message to be able to perform transactions in the marketplace.</p>
+      </div>
+      <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <div class="card-body">
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Address</span>
+            </label>
+            <input type="text" placeholder="ban_..." class="input input-bordered" />
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Signature</span>
+            </label>
+            <input type="text" placeholder="ABCX..." class="input input-bordered" />
+            <label class="label">
+              <a href="#" class="label-text-alt link link-hover">Not sure how to sign a message?</a>
+            </label>
+          </div>
+          <div class="form-control mt-6">
+            <button class="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
