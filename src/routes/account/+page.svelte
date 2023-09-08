a5f31f7b0b54da04c032330ea446682bf328a4e2
jetstream0 <49297268+jetstream0@users.noreply.github.com>
2023-09-08 01:19:32 -0400

    add clipboard copy

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { page } from '$app/stores';
+  import { onMount } from 'svelte';
 
   import AddressNfts from '$lib/components/AddressNfts.svelte';
   import { format_commas } from '$lib/utils/format.js';
@@ -9,6 +10,14 @@
 
   let balance_decimal = data.account?.network?.balance_decimal;
   balance_decimal = balance_decimal?.split(".")[0]+"."+balance_decimal?.split(".")[1].slice(0,2);
+
+  onMount(() => {
+    if (navigator?.clipboard) {
+      document.getElementById("copy-address").onclick = () => {
+        navigator.clipboard.writeText(data.account.address);
+      };
+    }
+  });
 </script>
 
 {#if data.success}
@@ -27,11 +36,11 @@
       <div class="flex-none self-center">
         <ul>
           <li class="tooltip tooltip-bottom" data-tip="Copy to clipboard">
-            <a class="btn btn-ghost btn-sm btn-circle" href="#">
+            <button id="copy-address" class="btn btn-ghost btn-sm btn-circle">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" />
               </svg>
-            </a>
+            </button>
           </li>
           <li class="tooltip tooltip-bottom" data-tip="Back" aria-current={$page.url.pathname === '/explorer' ? 'page' : undefined}>
             <a class="btn btn-ghost btn-sm btn-circle" href="/explorer">
