ca3bb1f19107def818ac518aadfbe91028b6e3e2
Jon Dough <49297268+stjet@users.noreply.github.com>
2024-03-14 23:06:16 +0000

    include official sales in header, minor improvements
    plus very minor outlining for official sales

--- src/routes/help/+page.svelte
+++ src/routes/help/+page.svelte
@@ -2,7 +2,9 @@
   export let data;
 </script>
 
-<ul>
+<p>A quick list of all help pages:</p>
+
+<ul class="list-disc pl-4">
   {#each data.help_pages as help_page}
     <li><a class="link" href="/help/{help_page}">{help_page}</a></li>
   {/each}
--- src/routes/market/official_sales/listings/+page.svelte
+++ src/routes/market/official_sales/listings/+page.svelte
@@ -1,7 +1,22 @@
 <script>
   export let success, official_sale, asset_supply, send_address;
 
-  //
+  //POST `${API_V1_PATH}/official_sales/:supplyHash/buy` for buy
 </script>
 
-<!---->
+
+<svelte:head>
+  <title>{success ? `${asset_supply.nft_metadata.name} from ${official_sale.collection}` : "Error"}</title>
+</svelte:head>
+
+<div class="flex flex-col w-full bg-base-300 rounded-box shadow p-4 my-4">
+  {#if !success}
+    <p class="mb-2"><span class="text-red-500">Error: Page Not Found</span></p>
+    <p>Please double check to see if the URL is correct, and that you are logged in.</p>
+  {:else}
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-5">
+      <!--placeholder-->
+    </div>
+  {/if}
+</div>
+
