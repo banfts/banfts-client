a91f523524ea4e1f27838653850e08919aa50163
stjet <49297268+stjet@users.noreply.github.com>
2024-11-13 18:39:10 +0000

    support bns domains in explorer search

--- src/lib/config/constants.js
+++ src/lib/config/constants.js
@@ -9,3 +9,10 @@ export const BANANO_PREFIX = 'ban_';
 export const PER_PAGE = 8;
 
 export const FOURTEEN_DAY_MS = 24*60*60*1000*14;
+
+export const TLD_MAPPING = {
+  "mictest": "ban_1dzpfrgi8t4byzmdeidh57p14h5jwbursf1t3ztbmeqnqqdcbpgp9x8j3cw6",
+  "jtv": "ban_3gipeswotbnyemcc1dejyhy5a1zfgj35kw356dommbx4rdochiteajcsay56",
+  "ban": "ban_1fdo6b4bqm6pp1w55duuqw5ebz455975o4qcp8of85fjcdw9qhuzxsd3tjb9",
+};
+
